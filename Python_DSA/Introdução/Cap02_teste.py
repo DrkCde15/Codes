9c7e5fa6b0015fc8cdba28d44a55d5f6a2ca730a
DrkCde15 <jcesarsantana215@gmail.com>
2024-10-28 13:43:00 -0300

    paralelogramo e calculadora

---
+++ Python_DSA/Introdução/Cap02_teste.py
@@ -0,0 +1,6 @@
+num = list(range(1, 101))
+
+div_por_4 = [num for num in num if num % 2 == 0 and num % 4 == 0]
+
+# Exibir a lista gerada
+print(div_por_4)
---
+++ Python_DSA/Introdução/Cap03/paralelogramo.py
@@ -0,0 +1,7 @@
+print('Bem-vindo ao calculador de área de paralelogramo')
+
+base = float(input("Insira o comprimento da base: "))
+altura = float(input("Insira a altura: "))
+area = base * altura
+
+print("Essa é a área: ", area)
