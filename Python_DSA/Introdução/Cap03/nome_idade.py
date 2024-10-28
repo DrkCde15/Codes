45f3c72d5a499b6c70c78e43f385018c77bd71fd
DrkCde15 <jcesarsantana215@gmail.com>
2024-10-28 13:50:48 -0300

    nome e idade

---
+++ Python_DSA/Introdução/Cap03/nome_idade.py
@@ -0,0 +1,7 @@
+print('Digite seu nome: ')
+nome = input()
+
+print('Digite sua idade: ')
+idade = input()
+
+print('Bem vindo ' + (nome) + ' que tem ' + (idade) + ' anos')
---
+++ Python_DSA/nome_idade/nome_idade.py
@@ -0,0 +1,9 @@
+#nome e idade usando input
+
+print('Digite seu nome: ')
+nome = input()
+
+print('Digite sua idade: ')
+idade = input()
+
+print('Bem vindo ' + (nome) + ' que tem ' + (idade) + ' anos')
