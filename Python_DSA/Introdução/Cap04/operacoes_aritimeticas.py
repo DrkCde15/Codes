c4c5e55f997f58b3013892dff2a53a551374990d
DrkCde15 <jcesarsantana215@gmail.com>
2024-10-30 09:31:54 -0300

    operacoes_aritimeticas fim

--- Python_DSA/Introdução/Cap04/operacoes_aritimeticas.py
+++ Python_DSA/Introdução/Cap04/operacoes_aritimeticas.py
@@ -1,17 +1,58 @@
-# Soma
-print(1 + 2)
+# # Soma
+# print(1 + 2)
 
-# Subtração
-print(6 - 2)
+# # Subtração
+# print(6 - 2)
 
-# Multiplicação
-print( 7 * 2)
+# # Multiplicação
+# print( 7 * 2)
 
-# Divisão
-print( 18 / 2)
+# # Divisão
+# print( 18 / 2)
 
-# Potência
-print(10 ** 2)
+# # Potência
+# print(10 ** 2)
 
-# Módulo
-print(10 % 3)
+# # Módulo
+# print(10 % 3)
+
+# #converção
+# resultado_float = float(9)
+# resultado_int_6_0 = int(6.0)
+# resultado_int_6_5 = int(6.5)
+
+# print("float(9):", resultado_float)
+# print("int(6.0):", resultado_int_6_0)
+# print("int(6.5):", resultado_int_6_5)
+
+# #hexadecimal e binário
+# result_hex = hex(394)
+# result_hex2 = hex(456)
+
+# result_bin = bin(286)
+# result_bin2 = bin(357)
+
+# print("hex(394)", result_hex)
+# print("hex(456)", result_hex2)
+# print("bin(286)", result_bin)
+# print("bin(357)", result_bin)
+
+#Funções abs, round e pow
+
+#Abs:
+result_abs = abs(-8)
+result_abs2 = abs(8)
+print("result_abs", result_abs)
+print("result_abs2", result_abs2)
+
+#Round:
+result_rnd = round(3.245657, 2)
+result_rnd2 = round(4.71836276, 3)
+print("result_rnd", result_rnd)
+print("result_rnd2", result_rnd2)
+
+#Pow = Potencia:
+result_pot = pow(4, 2)
+result_pot2 = pow(5, 3)
+print("result_pot", result_pot)
+print("result_pot2", result_pot2)
