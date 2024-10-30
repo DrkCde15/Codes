fc3669c5a3bf2e6afd171aec05568672094ab9ff
DrkCde15 <jcesarsantana215@gmail.com>
2024-10-30 10:28:09 -0300

    variaveis

---
+++ Python_DSA/Introdução/Cap04/variaveis_operadores.py
@@ -0,0 +1,46 @@
+#Declarando Variáveis
+var_teste = 1
+print(var_teste)
+print(type(var_teste))
+
+var_teste2 = 2.5
+print(var_teste2)
+print(type(var_teste2))
+
+var_teste3 = "j"
+print(var_teste3)
+print(type(var_teste3))
+
+
+#Declaração Múltipla
+p1, p2, p3, = "Caio,", "Alicia,", "Afonso"
+print(p1, p2, p3)
+
+
+#Transição de valores entre variáveis
+f1 = f2 = f3 = "Banana"
+print(f1)
+
+#Variáveis atribuidas a outras variáveis
+l = 2
+a = 4
+p = 2 * l + 2 * a
+
+print(p)
+
+#Operações com variáveis
+n1 = 18
+n2 = 2
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+print(n1 / n2)
+print(n1 ** n2)
+print(n1 % n2)
+
+#Concatenação de variáveis
+n = "Júlio"
+sn = "César"
+
+fN = n + " " + sn
+print(fN)
