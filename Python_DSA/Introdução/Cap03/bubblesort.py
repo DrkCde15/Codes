f768d415606ad1691072812e303a67455b5ef638
DrkCde15 <jcesarsantana215@gmail.com>
2024-11-07 09:01:19 -0300

    ateracoes

--- Python_DSA/Introdução/Cap03/bubblesort.py
+++ Python_DSA/Introdução/Cap03/bubblesort.py
@@ -1,8 +1,8 @@
-lista = [6, 3, 12, 7, 24, 67, 29, 55, 69, 34, 56]
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
+lista = [6, 3, 12, 7, 24, 67, 29, 55, 69, 34, 56] # lista de números
+def bubble_sort(arr): # função de ordenação bubblesort
+    n = len(arr) # tamanho da lista
+    for i in range(n): # loop de ordenação bubblesort
+        for j in range(0, n-i-1): # loop de ordenação bubblesort 
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 bubble_sort(lista)
