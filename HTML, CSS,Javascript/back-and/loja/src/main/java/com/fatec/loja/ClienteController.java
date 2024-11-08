f82b46d255d49414c52caf5b2367e6d55af76a12
DrkCde15 <jcesarsantana215@gmail.com>
2024-11-08 09:33:24 -0300

    back e front up

--- HTML, CSS,Javascript/back-and/loja/src/main/java/com/fatec/loja/ClienteController.java
+++ HTML, CSS,Javascript/back-and/loja/src/main/java/com/fatec/loja/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,7 +39,7 @@ public class ClienteController {
             return null;
         }
     }
-
+    @CrossOrigin(origins = "*")
     @DeleteMapping("/api/cliente/{codigo}")
     public String remover(@PathVariable int codigo) {
         if (bd.existsById(codigo)) {
