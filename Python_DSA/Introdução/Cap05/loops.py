7243d501577e454e4c1f1800d64f17ade57da596
DrkCde15 <jcesarsantana215@gmail.com>
2024-11-09 13:17:59 -0300

    loops e operadores_logs

---
+++ Python_DSA/Introdução/Cap05/loops.py
@@ -0,0 +1,47 @@
+# #loop for com tupla
+# tp = (2, 3, 4)
+# for i in tp:
+#     print(i)
+
+    
+# #for com lista de strings
+# tp = ["Afonso", "Alice", "Caio"]
+# for i in tp:
+#     print(i)
+    
+# #for com lista de inteiros
+# tp = [2, 3, 4]
+# for i in tp:
+#     print(i)
+
+# #for com dicionario
+# tp = {"Afonso": 19, "Alice": 20, "Caio": 21}
+# for i in tp:
+#     print(i, tp[i])
+
+# #for com range
+# #range: é uma funcao que cria uma sequencia de numeros inteiros
+# for i in range(0,11):
+#     print(i)
+    
+# #for com lista e if
+# tp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in tp:
+#     if i % 2 == 0:
+#         print(i)
+
+# #for in range com passos de 2 em 2
+# for i in range(0,103, 2):
+#     print(i)
+
+# #for caracter de uma string
+# for i in "Kali Linux":
+#     print(i)
+    
+
+#loop for aninhado
+lst_1 = [1, 2, 3]
+lst_2 = [4, 5, 6]
+for i in lst_1:
+    for j in lst_2:
+        print(i, j)
---
+++ Python_DSA/Introdução/Cap05/operadores_log.py
@@ -0,0 +1,45 @@
+#Operador logico and = etorna True se todas as condições forem verdadeiras
+#Operador logico or = retorna True se pelo menos uma das condições forem verdadeiras
+#Operador logico not = inverte o valor da condição
+
+# print(input("Informe seu nome: "))  # Solicita o nome do usuário
+
+# idade = int(input("Informe sua idade: "))  # Solicita a idade do usuário
+
+# if idade > 17:  # Se a idade for maior que 17
+#     print("Pode dirigir")  # Imprime "Pode dirigir"
+# else:  # Senão
+#     print("Não pode dirigir")  # Imprime "Não pode dirigir"
+    
+#Operador logico and
+nome = "João"
+idade = 18
+if idade >17 and nome == "João":
+    print("Pode dirigir")
+else:
+    print("Não pode dirigir")
+
+#operador logico or
+nome = "João"
+idade = 18
+if idade >17 or nome == "João":
+    print("Pode dirigir")
+else:
+    print("Não pode dirigir")
+    
+#operador logico not
+nome = "João"
+idade = 18
+if not idade >17 or nome == "Carlos":
+    print("Pode dirigir")
+else:   
+    print("Não pode dirigir")
+
+
+#operador logico not, and e or
+nome = "João" #Pode ser João ou Carlos
+idade = 18 #Pode ser 17 ou 18
+if not idade >17 and nome == "João" or nome == "Carlos": # se o nome for João ou Carlos e a idade for menor que 17 não pode dirigir
+    print("Pode dirigir") #Pode dirigir
+else:   #Senão
+    print("Não pode dirigir") #Não pode dirigir
