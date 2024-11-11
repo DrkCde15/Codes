890bb824d485d56f7c2e7fc46c60f19e7b121c32
DrkCde15 <jcesarsantana215@gmail.com>
2024-11-11 14:24:25 -0300

    aula do dia 07 arrumada

--- Java e JS (POO-PAOO)/JS_PAOO/microsservicos/observacoes/index.js
+++ Java e JS (POO-PAOO)/JS_PAOO/microsservicos/observacoes/index.js
@@ -1,47 +1,33 @@
-const express = require("express")
-const axios = require('axios')
-const { v4: uuidv4 } = require('uuid');
-const app = express();
-
-//função middleware
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+const app = express()
+//middleware
 app.use(express.json())
 
 const baseObservacoes = {}
 
-//POST /lembretes/id/observacoes
-app.post('/lembretes/:id/observacoes', async(req, res) => {
-    const idObs = uuidv4();
-    const { texto } = req.body
-    const observacoesDoLembrete = baseObservacoes[req.params.id] || []
-    const observacao = {
-        id: idObs,
-        texto: texto,
-        lembreteId: req.params.id,
-        status: 'pendente'
-    }
-    observacoesDoLembrete.push({id: idObs, texto: texto})
-    baseObservacoes[req.params.id] = observacoesDoLembrete
-    await axios.post('http://localhost:10000/eventos', {
-        type : 'ObservacaoCriada',
-        payload : observacao
-    })
-    res.status(201).json(observacoesDoLembrete)
+//POST /lembretes/1/observacoes
+app.post('/lembretes/:id/observacoes', (req, res) => {
+  const idObs = uuidv4()
+  const { texto } = req.body
+  const observacoesDoLembrete = baseObservacoes[req.params.id] || []
+  observacoesDoLembrete.push({id: idObs, texto: texto})
+  baseObservacoes[req.params.id] = observacoesDoLembrete
+  res.status(201).json(observacoesDoLembrete)
 })
 
+//GET /lembretes/1/observacoes
+app.get('/lembretes/:id/observacoes', (req, res) => {
+  res.json(baseObservacoes[req.params.id] || [])
+})
+
+//sua vez, faça
 app.post('/eventos', (req, res) => {
   const evento = req.body
   console.log(evento)
   res.end()
 })
-
-
 const port = 5000
 app.listen(port, () => {
-    console.log(`Observações. Porta ${port}.`)
+  console.log(`Observações. Porta ${port}.`)
 })
-
-//GET /lembreste/id/observacoes
-app.get('/lembretes/:id/observacoes', (req, res) => {
-    res.json(baseObservacoes[req.params.id] || [])
-})
-
