a3020bc6b5ad0529423cebf8846cb4abc7a7f4e6
DrkCde15 <jcesarsantana215@gmail.com>
2024-12-16 10:54:33 -0300

    bibliotecas

---
+++ Python_DSA/Introdução/Cap05/bibliotecas.py
@@ -0,0 +1,11 @@
+import math #biblioteca matematica
+import random #biblioteca aleatoria
+import datetime #biblioteca de datas
+
+print(math.sqrt(25))
+print(math.pi)
+
+print (random.randint(0, 100)) #gera um numero aleatorio entre 0 e 100
+
+agora = datetime.datetime.now()
+print(agora)
