589557ae3599b6a965943421d628e02318e9fb8a
JulioCesar <160616751+DrkCde15@users.noreply.github.com>
2025-07-11 15:22:17 -0300

    Update funcoes.py

--- Python_DSA/Introdução/Cap05/funcoes.py
+++ Python_DSA/Introdução/Cap05/funcoes.py
@@ -1,22 +1,22 @@
-# def primeiraFunc():
-#     print("hello world")
-# primeiraFunc()
+def primeiraFunc():
+    print("hello world")
+primeiraFunc()
 
-# def segundaFunc():
-#     nome = input("Digite seu nome: ")
-#     print("Ola " + nome)
-# segundaFunc()
+def segundaFunc():
+    nome = input("Digite seu nome: ")
+    print("Ola " + nome)
+segundaFunc()
 
-# def terceiraFunc():
-#     nome = input("Digite seu nome: ")
-#     idade = input("Digite sua idade: ")
-#     print("Ola " + nome + " e voce tem " + idade + " anos")
-# terceiraFunc()
+def terceiraFunc():
+    nome = input("Digite seu nome: ")
+    idade = input("Digite sua idade: ")
+    print("Ola " + nome + " e voce tem " + idade + " anos")
+terceiraFunc()
 
-# def imprimeNum():
-#     for i in range(0, 10):
-#         print("numero: " + str(i))
-# imprimeNum()
+def imprimeNum():
+    for i in range(0, 10):
+        print("numero: " + str(i))
+imprimeNum()
 
 
 def imprimeNumPares():
@@ -29,3 +29,25 @@ def imprimeNumImpares():
         print("numero: " + str(i))
 
 imprimeNumImpares()
+
+def identificar_tipo(valor):
+    if valor.lower() == 'true' or valor.lower() == 'false':
+        tipo = 'bool'
+    elif valor.lstrip('-').isdigit():
+        tipo = 'int'
+    elif '.' in valor:
+        partes = valor.split('.')
+        if len(partes) == 2 and (partes[0].lstrip('-').isdigit() and partes[1].isdigit()):
+            tipo = 'float'
+        else:
+            tipo = 'string'
+    elif len(valor) == 1:
+        tipo = 'char'
+    else:
+        tipo = 'string'
+
+    return tipo
+
+entrada = input("Digite algo: ")
+tipo = identificar_tipo(entrada)
+print(f"Tipo identificado: {tipo}")
