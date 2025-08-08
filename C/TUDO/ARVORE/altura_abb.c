3c93b98fff99d3cf1cc7d81cd36d72f0ee086e1b
JulioCesar <160616751+DrkCde15@users.noreply.github.com>
2025-08-08 13:22:24 -0300

    Delete C/TUDO directory

--- C/TUDO/ARVORE/altura_abb.c
+++
@@ -1,9 +0,0 @@
-//5. Escreva uma func¸ao que calcula a altura de uma arvore binaria.
-
-#include "abb.h"
-
-int main () {
-
-
-    return 0;
-}
--- C/TUDO/ARVORE/teste_no.c
+++
@@ -1,8 +0,0 @@
-#include "no.h"
-
-int main () {
-    t_no * no1 = constroi_no(12);
-    mostra_no(no1);
-
-    return 0;
-}
