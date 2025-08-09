83d69e97e05f6bdfb1cb19df833243655440ebcd
DrkCde15 <jcesarsantana215@gmail.com>
2025-08-09 11:56:24 -0300

    aula_00

--- C/ED/hello.c
+++
@@ -1,6 +0,0 @@
-//hello word em c
-#include <stdio.h>
-int main(){
-    printf("Hello word");
-    return 0;
-}
--- C/TUDO/PILHAS/pilha do afonso/pilhas/pilhas_sobre_lista_ligada/ex.c
+++
@@ -1,3 +0,0 @@
-//Lista 4 - 13, 16, 21, 24
-//Lista 6 - 5, 8, 13, 14 
-//Obs: dois com a pilha sobre vetor, dois com a pilha sobre lista ligada 
