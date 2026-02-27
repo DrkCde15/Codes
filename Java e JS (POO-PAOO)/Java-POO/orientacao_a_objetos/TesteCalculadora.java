5ef09f95b652910912c0ff5871db0c7528f36402
DrkCde15 <jcesarsantana215@gmail.com>
2026-02-27 15:50:15 -0300

    update repo

--- Java e JS (POO-PAOO)/Java-POO/orientacao_a_objetos/TesteCalculadora.java
+++
@@ -1,9 +0,0 @@
-public class TesteCalculadora{
-    public static void main(String [] args){
-        Calculadora calculadora = new Calculadora();
-        System.out.println(calculadora.somar(10,5));
-        System.out.println(calculadora.multiplicar(10, 5));
-        System.out.println(calculadora.subtrair(10, 5));
-        System.out.println(calculadora.dividir(10, 5));
-    }
-}
--- Java e JS (POO-PAOO)/Java-POO/orientacao_a_objetos/TesteLivroDeNotas.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class TesteLivroDeNotas {
-    public static void main(String [] args){
-        var livroDeNotas = new LivroDeNotas(JOptionPane.showInputDialog("Insira o nome da matéria aqui"));
-        livroDeNotas.enviarMensagem();
-    }
-}
