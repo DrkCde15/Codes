2ccd6c09871707c9ed350fac0ea26fa84d849c4e
DrkCde15 <jcesarsantana215@gmail.com>
2026-03-06 17:54:50 -0300

    feat: adiciona componentes Pedido, Cartao e Feedback

--- Java-JS-JSX (POO-PAOO-PDM)/JSX_PDM/ap04-props/src/App.jsx
+++ Java-JS-JSX (POO-PAOO-PDM)/JSX_PDM/ap04-props/src/App.jsx
@@ -1,5 +1,20 @@
-const App = () => (
-  <div className="container border mt-2">
+import Pedido from "./Pedido"
+import Cartao from "./Cartao"
+import Feedback from "./Feedback"
+const App = () => {
+  const textoOK = "Já recebi"
+  const textoNOK = "Ainda não recebi"
+  const funcaoOK = () => alert('Agradecemos o feedback')
+  const funcaoNOK = () => alert('Verificaremos')
+  const componenteFeedback = (
+    <Feedback 
+      textoOK={textoOK}
+      textoNOK={textoNOK}
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+    />
+  )
+  return <div className="container border mt-2">
 
     <div className="row">
       <div className="col-12">
@@ -11,20 +26,46 @@ const App = () => (
     <div className="row">
       {/* .col-sm-12.col-lg-6.col-xxl-3*4    */}
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <div className="py-2 border">Pedido 1</div>
+        <Cartao cabecalho="22/04/2026">
+          <Pedido
+            data="15/06/2024"
+            icone="alarm-clock"
+            titulo="Despertador Analógico"
+            descricao="Um despertador analógico" />
+          {componenteFeedback}
+        </Cartao>
+
       </div>
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <div className="py-2 border">Pedido 2</div>
+        <Cartao>
+          <Pedido data="17/05/2024"
+            icone="shirt"
+            titulo="Camisa"
+            descricao="Uma camisa preta" />
+          {componenteFeedback}
+        </Cartao>
       </div>
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <div className="py-2 border">Pedido 3</div>
+        <Cartao>
+          <Pedido data="14/06/2024"
+            icone="bowling-ball"
+            titulo="Bola de boliche"
+            descricao="Uma bola de boliche" />
+          {componenteFeedback}
+        </Cartao>
       </div>
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <div className="py-2 border">Pedido 4</div>
-       </div>   
+        <Cartao>
+          <Pedido data="15/06/2024"
+            icone="bicycle"
+            titulo="Bicicleta"
+            descricao="Bicicleta 18 marchas" />
+          {componenteFeedback}
+        </Cartao>
+      </div>
     </div>
   </div>
-)
+}
 
 
 export default App
